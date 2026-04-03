3c5e89fdc743788935cf96622d51401dafa2fcb5
Nayan Kumar Raj <nkrtechnkr@gmail.com>
2026-04-04 00:46:13 +0530

    chore: init spring boot project via spring initializr

---
+++ src/test/java/com/finvault/FinVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
