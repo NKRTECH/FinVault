ebc6f1fc85be9332efa021356f0dbf990224d9cc
Nayan Kumar Raj <nkrtechnkr@gmail.com>
2026-04-04 19:21:33 +0530

    feat(repo): add repository interfaces

---
+++ src/main/java/com/finvault/repository/FinancialRecordRepository.java
@@ -0,0 +1,10 @@
+package com.finvault.repository;
+
+import com.finvault.entity.FinancialRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FinancialRecordRepository extends JpaRepository<FinancialRecord, Long>, JpaSpecificationExecutor<FinancialRecord> {
+}
